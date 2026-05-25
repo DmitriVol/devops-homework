ecf21d33791428c44a9582168ad4b36da7b40f69
DmitriVol <dvolotchaev@gmai.com>
2026-05-25 23:33:59 +0300

    tf: provision DynamoDB table with server-side encryption

---
+++ terraform/dynamodb.tf
@@ -0,0 +1,14 @@
+resource "aws_dynamodb_table" "requests" {
+  name         = "${var.environment}-requests-db"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  server_side_encryption {
+    enabled = true
+  }
+}
